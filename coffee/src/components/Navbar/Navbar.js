868f2cfe98a2a63f4bdf5b955682cfb7cf4f6d03
Dav-lopez <78981910+Dav-lopez@users.noreply.github.com>
2022-04-26 11:44:06 +0100

    littlv chang

--- coffee/src/components/Navbar/Navbar.js
+++ coffee/src/components/Navbar/Navbar.js
@@ -1,19 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import logo from '../img/logo4.webp'
+import './Navbar.css'
 function Navbar() {
   return (
     <div className='navibar'>
-        <div className=''>
-           Logo Image
-        </div>
-        <div className='navi-links'>
-           <ul>
-                <li> Home </li>
-                <li> Product </li>
-                <li> About </li>
-                <li> Contact </li>
-            </ul> 
-        </div>
+        <nav className='logo'>
+        <img src={logo} alt="Logo" />;
+        </nav>
+        <nav className='navi-links'>
+        <div className="links">
+              
+              <Link className='li' to="/home">Home</Link>
+              <Link className='li' to="/about">About</Link>
+              <Link className='li' to="/product">Product</Link>
+              <Link className='li' to="/contact">Contact</Link>
+                
+  
+              </div>
+        </nav>
     </div>
   )
 }
