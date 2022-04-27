5ecc8cb92912e99d1bb4f37bb724f2509e74ab79
Dav-lopez <78981910+Dav-lopez@users.noreply.github.com>
2022-04-27 23:18:17 +0100

    1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ function About() {
              <img src={cf} alt="Logo" />
              </div>
               <div className='contnet'>
-                <h3>This valuable experience taught me how to lead a group of individuals and provided me with a great opportunity to work and collaborate with people from various cultures and countries. What's more, I learned how to work and perform well in a team setting and further developed my knowledge of different aspects of a business</h3>
+                <h3>This valuable experience taught met how to lead a group of individuals and provided me with a great opportunity to work and collaborate with people from various cultures and countries. What's more, I learned how to work and perform well in a team setting and further developed my knowledge of different aspects of a business</h3>
                   <p>
                   This valuable experience taught me how to lead a group of individuals and provided me with a great opportunity to work and collaborate with people from various cultures and countries. What's more, I learned how to work and perform well in a team setting and further developed my knowledge of different aspects of a business
                   </p><p>
