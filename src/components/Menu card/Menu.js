3fc98816aa6c460949946d4f3bf90001d9bd04e9
Dav-lopez <78981910+Dav-lopez@users.noreply.github.com>
2022-05-08 11:58:34 +0100

    product2

--- src/components/Menu card/Menu.js
+++ src/components/Menu card/Menu.js
@@ -2,12 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import t1 from '../img/cf3.jpg'
 import t2 from '../img/cf6.jpg'
+import t3 from '../img/product1.jpg'
+import t4 from '../img/product2.jpg'
+import t5 from '../img/product3.jpg'
+import t6 from '../img/product4.jpg'
+import t7 from '../img/product5.jpg'
+import t8 from '../img/product6.jpg'
 function Menu() {
 
   const drinks = [
     
-      {id : 1 , drinkname : 'black', image : t1, price : '$10'},
-      {id : 2 , drinkname : 'grn', image : t2, price : '$20'}
+      {id : 1 , drinkname : 'Black', image : t1, price : '$10'},
+      {id : 2 , drinkname : 'Green', image : t2, price : '$20'},
+      {id : 3 , drinkname : 'Sweet', image : t3, price : '$10'},
+      {id : 4 , drinkname : 'Mango Flavour', image : t4, price : '$20'},
+      {id : 5 , drinkname : 'orange', image : t5, price : '$10'},
+      {id : 6 , drinkname : 'chooco', image : t6, price : '$20'},
+      {id : 7 , drinkname : 'sucose', image : t7, price : '$10'},
+      {id : 8 , drinkname : 'lamora', image : t8, price : '$20'},
     
   ]
 
